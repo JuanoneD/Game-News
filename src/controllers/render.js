8d8d7d32f48390cb10c1f629062adc197f254a14
JuanOneD <Juandiego.jd42@gmail.com>
2024-08-29 09:39:49 -0300

    Finish Header_0.2

--- src/controllers/render.js
+++ src/controllers/render.js
@@ -1,9 +1,10 @@
 const articles = require('../model/article');
 const users = require('../model/user');
+const renders = require('../config/renders');
 
 module.exports = {
     async pagInicialGet(req, res){
-        res.render('../views/index',{login:null,error:null});
+        renders.renderIndex(res);
     },
     async pagUpdate(req,res){
         let id = req.params.id
