9ba1c14f0f76947b1b0125ea226441cf708f79a2
JuanOneD <Juandiego.jd42@gmail.com>
2024-08-30 09:13:15 -0300

    Giving up on login features

--- public/script/index.js
+++ public/script/index.js
@@ -5,17 +5,3 @@ function errorclick(){
     document.getElementById('inputerror').click();
 }
 
-
-function saveLogin(){
-    let email = document.getElementById("login_email").value;
-    let password = document.getElementById("login_password").value;
-    password = createHash('sha256').update(password).digest('hex');
-
-    let login = [];
-
-    login.push(email);
-    login.push(password);
-
-    
-    localStorage.setItem("login", JSON.stringify(login));
-}
